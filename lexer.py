f0a2497727c5507eb7daad14079d5c04e4fd2093
viny.tito <viny.palazuelos@gmail.com>
2025-10-29 20:08:20 -0700

    feat(lexer): añadir tokens para operadores booleanos, comparaciones, aritmética y delimitadores
    Se agregan patrones regex y tokens para:
    - Operadores booleanos: and, or, not
    - Operadores de comparación: ==, !=, <=, >=, <, >
    - Operaciones aritméticas y asignación: +, -, *, /, =
    - Delimitadores y manejo de espacios y nuevas líneas

--- lexer.py
+++ lexer.py
@@ -7,22 +7,60 @@ class Token:
     value: str
 
 def lexer(text):
+    line_num = 1
+    line_start = 0
+
     token_spec = [
         ('NUMBER', r'\d+'),
-        ('PRINT',  r'print'),
+        ('PRINT',  r'\bprint\b'),
         ('ID',     r'[A-Za-z_]\w*'),
+
+        # operadores booleanos
+        ('AND', r'\band\b'),
+        ('OR', r'\bor\b'),
+        ('NOT', r'\bnot\b'),
+
+        # operadores logicos
+
+        ("EQ", r'=='),
+        ("NEQ", r'!='),
+        ("LTE", r'<='),
+        ("GTE", r'>='),
+        ('LT', r'<'),
+        ('GT', r'>'),
+
+        #Operaciones aritmeticas y asignacion
+
         ('ASSIGN', r'='),
         ('PLUS',   r'\+'),
         ('MINUS',  r'-'),
+        ('DIV', r'/'),
+        ('MULT', r'*'),
+
+        # Delimitadores
+
+        ('LPAREN', r'\('),
+        ('RPAREN', r'\)'),
+        ('LBRACE', r'\{'),
+        ('RBRACE', r'\}'),
+        ('COMMA', r','),
+        ('SEMI', r';'),
+
         ('NEWLINE', r'\n'),
         ('SKIP',   r'[ \t]+'),
-        ('DIV',r'/'),
-        ('MULT',r'*'),
+
     ]
     tok_regex = '|'.join(f'(?P<{name}>{pattern})' for name, pattern in token_spec)
+
     for mo in re.finditer(tok_regex, text):
         kind = mo.lastgroup
         value = mo.group()
-        if kind == 'SKIP':
+
+        if kind in ('SKIP', 'COMMENT'):
             continue
+        if kind == 'MISMATCH':
+            raise SyntaxError(f'Carácter inesperado: {value}')
+        if kind == 'KEYWORD':
+            kind = value.upper()  # print -> PRINT
+
         yield Token(kind, value)
